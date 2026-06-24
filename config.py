1a01bf963cfadd7815b396779bc3f0b670f307cd
sardorbek070070-jpg <sardorbek070070@gmail.com>
2026-06-24 16:14:09 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 BOT_TOKEN = "8729235570:AAG1xPUrBK3Kc4tNyEKwZo7yy-jttv2hxyQ"
 ADMIN_ID = 2010030869          # o'z ID'ingiz
-DB_PATH = "postgresql://kino_8beg_user:0jaK2UwP2i5BHD4CgakZKWTU43cfSNnL@dpg-d85go7hkh4rs73dtrdag-a.oregon-postgres.render.com/kino_8beg"            # SQLite fayli nomi
+# PostgreSQL sozlamalari
+DATABASE_URL = "postgresql://user:password@host:port/dbname"
